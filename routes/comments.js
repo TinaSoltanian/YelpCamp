ac8edfcf01a143a8e24b22a690cb311cd60f72bf
tinasoltanian <tinasoltanian@gmail.com>
2017-12-27 21:37:36 +0000

    add edit commit route and page without authorization for now

--- routes/comments.js
+++ routes/comments.js
@@ -37,6 +37,28 @@ router.post("/",isLoggedIn, function (req, res) {
     })
 })
 
+//edit comment route
+router.get("/:comment_id/edit", function(req, res){
+    Comment.findById(req.params.comment_id, function(err, comment) {
+        if(err){
+            res.redirect("back")
+        } else {
+           res.render("comments/edit", {campground_id: req.params.id, comment: comment})      
+        }
+    })
+})
+
+//update edited comment
+router.put("/:comment_id", function(req, res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+        if(err){
+            res.redirect("back")
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    })
+})
+
 function isLoggedIn(req, res, next){
     if (req.isAuthenticated()){
         return next();
