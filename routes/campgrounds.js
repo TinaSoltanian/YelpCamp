d899fa1c94d49d9323a5f63842d18cc78be689b7
tinasoltanian <tinasoltanian@gmail.com>
2017-12-27 08:30:52 +0000

    add campground delete method and route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -74,6 +74,17 @@ router.put("/:id", function(req, res){
     })
 })
 
+//delete campground
+router.delete("/:id", function(req, res){
+    Campground.findByIdAndRemove(req.params.id, function(err){
+        if (err){
+            res.redirect("/campgrounds")
+        } else {
+            res.redirect("/campgrounds")
+        }
+    })
+})
+
 function isLoggedIn(req, res, next){
     if (req.isAuthenticated()){
         return next();
